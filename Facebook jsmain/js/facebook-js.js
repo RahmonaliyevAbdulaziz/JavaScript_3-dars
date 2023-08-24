6de798e9dac35ceae02e91fdd1b156882e20534f
RahmonaliyevAbdulaziz <rahmonaliyevabdulaziz2007@gmail.com>
2023-08-24 12:46:01 +0500

    JavaScript_3-dars

---
+++ Facebook jsmain/js/facebook-js.js
@@ -0,0 +1,151 @@
+let wrap = document.getElementsByTagName("div").namedItem("wrap");
+let formGroup = document.getElementsByTagName("div").namedItem("form-group");
+let head = document.getElementsByTagName("h1").namedItem("head")
+let head2 = document.getElementsByTagName("h4").namedItem("head2")
+let name = document.getElementsByTagName("input").namedItem("name");
+let sourname = document.getElementsByTagName("input").namedItem("sourname");
+let hr = document.getElementsByTagName("hr").namedItem("hr");
+let number = document.getElementsByTagName("input").namedItem("number");
+let password = document.getElementsByTagName("input").namedItem("password");
+let span = document.getElementsByTagName("span").namedItem("span");
+let day = document.getElementsByTagName("select").namedItem("day");
+let month = document.getElementsByTagName("select").namedItem("month");
+let year = document.getElementsByTagName("select").namedItem("year");
+let span2 = document.getElementsByTagName("span").namedItem("span2");
+let pol1 = document.getElementsByTagName("div").namedItem("pol1");
+let pol2 = document.getElementsByTagName("div").namedItem("pol2");
+let pol3 = document.getElementsByTagName("div").namedItem("pol3");
+let inputRadio = document.getElementsByTagName("input").namedItem("jenshina")
+let inputRadio2 = document.getElementsByTagName("input").namedItem("mujshina")
+let inputRadio3 = document.getElementsByTagName("input").namedItem("drugoe")
+let p1 = document.getElementsByTagName("p").namedItem("p1");
+let submit = document.getElementsByTagName("button").namedItem("submit");
+let body = document.getElementsByTagName("body").namedItem("body");
+
+body.style.backgroundColor = "lightgray";
+
+submit.style.boxSizing = "border-box";
+submit.style.backgroundColor = "green";
+submit.style.color = "white";
+submit.style.padding = "0.5rem 1.5rem";
+submit.style.fontSize = "20px";
+submit.style.letterSpacing = "3px";
+submit.style.border = "none";
+submit.style.borderRadius = "7px";
+submit.style.margin = "1rem 0 1rem 6rem";
+
+
+
+
+p1.style.fontSize = "11px";
+p1.style.color = "gray";
+p2.style.color = "gray";
+p2.style.fontSize = "11px";
+
+inputRadio.style.float = "right";
+inputRadio2.style.float = "right";
+inputRadio3.style.float = "right";
+
+pol1.style.width = "30.65%";
+pol1.style.boxSizing = "border-box";
+pol1.style.padding = "0.70rem 0 0.7rem 0.5rem"
+pol1.style.border = "1px solid lightgray"
+pol1.style.borderRadius = "5px";
+pol1.style.marginRight = "0.5rem";
+pol1.style.display = "inline-block"
+
+pol2.style.width = "30.65%";
+pol2.style.boxSizing = "border-box";
+pol2.style.padding = "0.70rem 0 0.7rem 0.5rem"
+pol2.style.border = "1px solid lightgray"
+pol2.style.borderRadius = "5px";
+pol2.style.marginRight = "0.51rem";
+pol2.style.display = "inline-block"
+
+pol3.style.width = "30.65%";
+pol3.style.boxSizing = "border-box";
+pol3.style.padding = "0.70rem 0 0.7rem 0.5rem"
+pol3.style.border = "1px solid lightgray"
+pol3.style.borderRadius = "5px";
+pol3.style.marginRight = "0.5rem";
+pol3.style.display = "inline-block"
+
+
+day.style.boxSizing = "border-box";
+day.style.padding = "0.70rem 0.2rem";
+day.style.width = "31.3%";
+day.style.border = "1px solid lightgray";
+day.style.borderRadius = "5px";
+day.style.marginRight = "0.5rem";
+
+month.style.boxSizing = "border-box";
+month.style.padding = "0.70rem 0.2rem";
+month.style.width = "31.3%";
+month.style.border = "1px solid lightgray";
+month.style.borderRadius = "5px";
+month.style.marginRight = "0.5rem";
+
+year.style.boxSizing = "border-box";
+year.style.padding = "0.70rem 0.2rem";
+year.style.width = "31.3%";
+year.style.border = "1px solid lightgray";
+year.style.borderRadius = "5px";
+
+
+span.style.fontSize = "12px";
+span.style.color = "gray"
+span.style.display = "block";
+span.style.margin = "1rem 0 0.50rem 0";
+span2.style.fontSize = "12px";
+span2.style.color = "gray"
+span2.style.display = "block";
+span2.style.margin = "1rem 0 0.50rem 0";
+password.style.width = "100%";
+password.style.boxSizing = "border-box";
+password.style.padding = "0.80rem";
+password.style.backgroundColor = "#eeeded";
+password.style.borderRadius = "5px";
+password.style.border = "none";
+password.style.marginTop = "0.50rem";
+
+number.style.width = "100%";
+number.style.boxSizing = "border-box";
+number.style.padding = "0.80rem";
+number.style.backgroundColor = "#eeeded";
+number.style.borderRadius = "5px";
+number.style.border = "none";
+number.style.marginTop = "0.50rem";
+
+wrap.style.width = "1340px";
+wrap.style.backgroundColor = "lightgray";
+wrap.style.height = "auto";
+wrap.style.borderRadius = "10px";
+wrap.style.padding = "5rem 0 4rem 0";
+
+formGroup.style.borderRadius = "5px";
+formGroup.style.width = "430px";
+formGroup.style.backgroundColor = "white";
+formGroup.style.margin = "20px auto";
+formGroup.style.padding = "0.05rem 1rem";
+formGroup.style.boxSizing = "border-box";
+
+head.style.textAlign = "left";
+hr.style.width = "107.2%";
+hr.style.marginLeft = "-1rem"
+head2.style.color = "lightgray";
+head2.style.marginTop = "-20px";
+
+name.style.padding = "0.80rem";
+name.style.boxSizing = "border-box";
+name.style.width = "48.5%";
+name.style.borderRadius = "5px";
+name.style.border = "none";
+name.style.backgroundColor = "#eeeded";
+name.style.marginRight = "0.5rem";
+
+sourname.style.padding = "0.80rem 0.80rem";
+sourname.style.boxSizing = "border-box";
+sourname.style.width = "48.5%";
+sourname.style.borderRadius = "5px";
+sourname.style.border = "none";
+sourname.style.backgroundColor = "#eeeded";
---
+++ js/JavaScript-main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+let wrap = document.getElementsByTagName("div").namedItem("wrap");
+let facebook = document.getElementsByTagName("img").namedItem("facebook-logo");
+let name = document.getElementById("sozdat-akk");
+
+wrap.style.backgroundColor = "red";
+wrap.style.width = "1200px";
+wrap.style.height = "2000px";
+
+facebook.style.width = "200px";
+facebook.style.height = "50px";
+
+
+name.style.fontSize = "10px";
+name.style.marginLeft = "200px";
